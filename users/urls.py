8573906626b7a6aeffb7759f27cd5c102619d4b0
Freshtuna <jun17114@gmail.com>
2021-02-02 13:39:09 +0900

    Add: social login, password reset
    Fix: password match

--- users/urls.py
+++ users/urls.py
@@ -5,11 +5,15 @@ from .views import (
     SignInView,
     MasterSignUpView,
     CategoryServiceView,
+    PasswordResetView,
+    KaKaoView,
 )
 
 urlpatterns = [
     path('/signup',SignUpView.as_view()),
     path('/signin',SignInView.as_view()),
     path('/master_signup',MasterSignUpView.as_view()),
-    path('/category',CategoryServiceView.as_view())
+    path('/category',CategoryServiceView.as_view()),
+    path('/password_reset',PasswordResetView.as_view()),
+    path('/kakao_signin',KaKaoView.as_view())
 ]   
--- users/views.py
+++ users/views.py
@@ -2,12 +2,15 @@ import json
 import bcrypt
 import jwt
 import re
-from datetime               import datetime 
+from datetime               import datetime, timedelta
 
 from django.http            import JsonResponse
 from django.views           import View
+from django.core            import mail
+from django.template.loader import render_to_string
+from django.utils.html      import strip_tags
 
-from my_settings            import SECRET_KEY, ALGORITHM
+from my_settings            import SECRET_KEY, ALGORITHM, EMAIL
 from users.models           import User, Master, Region, SubRegion, Gender, MasterService
 from services.models        import Service, Category
 from users.utils            import (
@@ -150,3 +153,125 @@ class CategoryServiceView(View):
             return JsonResponse({'MESSAGE': 'JSON_DECODE_ERROR'}, status=400)
         except TypeError:
             return JsonResponse({'MESSAGE': 'TYPE_ERROR'}, status=400)
+            
+class KaKaoView(View):
+
+    def get(self, request):
+        try: 
+            kakao_token     = request.headers.get('Authorization', None)
+            url             = "https://kapi.kakao.com/v2/user/me"
+            headers         = {'Authorization': f'Bearer {kakao_token}'}
+            kakao_data      = requests.get(url ,headers=headers).json()
+            hashed_password = bcrypt.hashpw(token_urlsafe()[:10].encode(), bcrypt.gensalt())
+            gender_dict     = {'male':'남자', 'female':'여자'}
+            gender          = Gender.objects.get(name=gender_dict[kakao_data['kakao_account']['profile']['gender']])
+
+            user, created = User.objects.get_or_create(
+                email    = kakao_data['kakao_account']['email'],
+                name     = kakao_data['kakao_account']['profile']['nickname'],
+                gender   = gender,
+                password = hashed_password.decode()
+            )
+            user_token = jwt.encode({"user_id":user.id}, SECRET_KEY, ALGORITHM)
+        
+            return JsonResponse({'MESSAGE': 'SUCCESS', 'token':kakao_data}, status=200)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'MESSAGE': 'JSON_DECODE_ERROR'}, status=400)
+
+class PasswordResetView(View):
+
+    def post(self, request):
+        try:
+            data  = json.loads(request.body)
+            email = data['email']
+
+            if not User.objects.filter(email=email).exists():
+                return JsonResponse({'MESSAGE': 'EMAIL_DOSENT_EXIST'}, status=400)
+
+            if not validate_email(email):
+                return JsonResponse({'MESSAGE':'INVALID_EMAIL'}, status=400)
+
+            user_info    = User.objects.get(email=email)
+            expire_date  = datetime.now() + timedelta(days=1)
+            token        = jwt.encode(
+                {'user_id':user_info.id,'expired_at':str(expire_date)},
+                SECRET_KEY,
+                algorithm=ALGORITHM
+            )
+            html_content = render_to_string('email.html',{
+                'user_email':email,
+                'user_token':f"http://localhost:3000/reset/{token}"
+            })
+
+            text_content = strip_tags(html_content)
+            msg = mail.EmailMultiAlternatives(
+                "Password Reset",
+                text_content,
+                EMAIL['EMAIL_HOST_USER'],
+                [ email ]
+            )
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+            return JsonResponse({'MESSAGE': 'EMAIL_SENDED'}, status=200)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'MESSAGE': 'JSON_DECODE_ERROR'}, status=400)
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+        except TypeError:
+            return JsonResponse({'MESSAGE': 'TYPE_ERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'MESSAGE': 'VALUE_ERROR'}, status=400)
+
+    def get(self, request):
+        try:
+            token         = request.headers.get('token',None)
+            decoded_token = jwt.decode(
+                token,
+                SECRET_KEY,
+                algorithms = ALGORITHM
+            )
+            if not User.objects.filter(id=decoded_token['user_id']).exists():
+                return JsonResponse({'MESSAGE': 'WRONG_TOKEN', 'valid':False}, status=400)
+
+            if str(datetime.datetime.now()) > decoded_token['expired_at']:
+                return JsonResponse({'MESSAGE': 'WRONG_TOKEN', 'valid':False}, status=400)
+
+            return JsonResponse({'MESSAGE': 'SUCCESS', 'valid':True})
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'MESSAGE': 'JSON_DECODE_ERROR'}, status=400)
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+        except TypeError:
+            return JsonResponse({'MESSAGE': 'TYPE_ERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'MESSAGE': 'VALUE_ERROR'}, status=400)
+            
+    def patch(self, request):
+        try:
+            data          = json.loads(request.body)
+            token         = bytes(data['token'], 'utf-8')
+            password      = data['password']
+
+            decoded_token = jwt.decode(
+                token,
+                SECRET_KEY,
+                algorithms = ALGORITHM
+            )
+
+            if decoded_token['expired_at'] < str(datetime.now()):
+                return JsonResponse({'MESSAGE': 'LINK_EXPIRED'}, status=400)
+
+            user          = User.objects.get(id=decoded_token['user_id'])
+            user.password = bcrypt.hashpw(password['resetPassword'].encode(), bcrypt.gensalt()).decode()
+            user.save()
+
+            return JsonResponse({'MESSAGE': 'PASSWORD_CHANGED'}, status=200)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'MESSAGE': 'JSON_DECODE_ERROR'}, status=400)
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+        except TypeError:
+            return JsonResponse({'MESSAGE': 'TYPE_ERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'MESSAGE': 'VALUE_ERROR'}, status=400)
